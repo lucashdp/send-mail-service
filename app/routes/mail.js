7d93fae2ed194f274ee6863548d9ef34753b090c
Lucas Duarte <lucashdp@outlook.com>
2018-10-20 22:02:35 -0300

    Implements the nodemailer to send mail

--- app/routes/mail.js
+++ app/routes/mail.js
@@ -1,8 +1,33 @@
-module.exports = function (app, db) {
+module.exports = function (app) {
     //----------POST---------------------------//
     app.post('/v1/public/mails', (req, res) => {
         const newMail = req.body.mail;
-        
-        console.log(JSON.stringify(newMail));
+
+        var nodemailer = require('nodemailer');
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: '****@gmail.com',
+                pass: '*****'
+            }
+        });
+
+        var mailOptions = {
+            from: 'lucsgp10@gmail.com',
+            to: 'lucashanryque@hotmail.com',
+            subject: 'Sending Email using Node.js',
+            text: 'That was easy!'
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+        res.send('Email enviado !!');
     });
 };
